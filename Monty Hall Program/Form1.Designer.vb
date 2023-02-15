554ad06f85ea4b965c24704ca74b7c63bfc94aa3
Zachary Long <zacl.attack.710@gmail.com>
2023-02-15 10:02:07 -0500

    Add project files.

---
+++ Monty Hall Program/Form1.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnDoor3 = New System.Windows.Forms.Button()
+        Me.btnDoor2 = New System.Windows.Forms.Button()
+        Me.btnDoor1 = New System.Windows.Forms.Button()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.switchcount = New System.Windows.Forms.Label()
+        Me.totalgames = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Reset = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.winscount = New System.Windows.Forms.Label()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnDoor3
+        '
+        Me.btnDoor3.AccessibleDescription = ""
+        Me.btnDoor3.Cursor = System.Windows.Forms.Cursors.Default
+        Me.btnDoor3.Location = New System.Drawing.Point(324, 318)
+        Me.btnDoor3.Name = "btnDoor3"
+        Me.btnDoor3.Size = New System.Drawing.Size(150, 25)
+        Me.btnDoor3.TabIndex = 2
+        Me.btnDoor3.Text = "Door 3"
+        Me.btnDoor3.UseVisualStyleBackColor = True
+        '
+        'btnDoor2
+        '
+        Me.btnDoor2.Location = New System.Drawing.Point(168, 318)
+        Me.btnDoor2.Name = "btnDoor2"
+        Me.btnDoor2.Size = New System.Drawing.Size(150, 25)
+        Me.btnDoor2.TabIndex = 1
+        Me.btnDoor2.Text = "Door 2"
+        Me.btnDoor2.UseVisualStyleBackColor = True
+        '
+        'btnDoor1
+        '
+        Me.btnDoor1.Location = New System.Drawing.Point(12, 318)
+        Me.btnDoor1.Name = "btnDoor1"
+        Me.btnDoor1.Size = New System.Drawing.Size(150, 25)
+        Me.btnDoor1.TabIndex = 0
+        Me.btnDoor1.Text = "Door 1"
+        Me.btnDoor1.UseVisualStyleBackColor = True
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Monty_Hall_Program.My.Resources.Resources.doors_closed_rectangular_red_2_panel_door_illustration2
+        Me.PictureBox3.Location = New System.Drawing.Point(324, 12)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(150, 300)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 15
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.Monty_Hall_Program.My.Resources.Resources.doors_closed_rectangular_red_2_panel_door_illustration2
+        Me.PictureBox2.Location = New System.Drawing.Point(168, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(150, 300)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 14
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Monty_Hall_Program.My.Resources.Resources.doors_closed_rectangular_red_2_panel_door_illustration2
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(150, 300)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 13
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.Monty_Hall_Program.My.Resources.Resources.GOAT
+        Me.PictureBox6.Location = New System.Drawing.Point(324, 12)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(150, 300)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 12
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.Monty_Hall_Program.My.Resources.Resources.GOAT
+        Me.PictureBox5.Location = New System.Drawing.Point(168, 12)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(150, 300)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 11
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Monty_Hall_Program.My.Resources.Resources.GOAT
+        Me.PictureBox4.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(150, 300)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 7
+        Me.PictureBox4.TabStop = False
+        '
+        'switchcount
+        '
+        Me.switchcount.Location = New System.Drawing.Point(480, 150)
+        Me.switchcount.Name = "switchcount"
+        Me.switchcount.Size = New System.Drawing.Size(100, 40)
+        Me.switchcount.TabIndex = 16
+        '
+        'totalgames
+        '
+        Me.totalgames.Location = New System.Drawing.Point(480, 190)
+        Me.totalgames.Name = "totalgames"
+        Me.totalgames.Size = New System.Drawing.Size(100, 40)
+        Me.totalgames.TabIndex = 17
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(480, 177)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 13)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "Total games : "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(480, 137)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 13)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Switches : "
+        '
+        'Reset
+        '
+        Me.Reset.Location = New System.Drawing.Point(481, 13)
+        Me.Reset.Name = "Reset"
+        Me.Reset.Size = New System.Drawing.Size(117, 121)
+        Me.Reset.TabIndex = 20
+        Me.Reset.Text = "&RESET"
+        Me.Reset.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(628, 80)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 21
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(480, 217)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 23
+        Me.Label2.Text = "Wins : "
+        '
+        'winscount
+        '
+        Me.winscount.Location = New System.Drawing.Point(480, 230)
+        Me.winscount.Name = "winscount"
+        Me.winscount.Size = New System.Drawing.Size(100, 40)
+        Me.winscount.TabIndex = 22
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(806, 378)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.winscount)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Reset)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.totalgames)
+        Me.Controls.Add(Me.switchcount)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.btnDoor3)
+        Me.Controls.Add(Me.btnDoor2)
+        Me.Controls.Add(Me.btnDoor1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnDoor3 As Button
+    Friend WithEvents btnDoor2 As Button
+    Friend WithEvents btnDoor1 As Button
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents switchcount As Label
+    Friend WithEvents totalgames As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Reset As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents winscount As Label
+End Class
---
+++ Monty Hall Program/Form1.vb
@@ -0,0 +1,196 @@
+﻿Public Class Form1
+    Dim counter As Integer = 0
+    Dim choice As Integer
+    Dim choice2 As Integer
+    Dim Switchdoor As Boolean
+    Dim WIN As Integer
+    Dim switchWIN As Integer
+    Dim loss As Integer
+    Dim switchloss As Integer
+    Dim rand As New Random
+    Dim random2 As Integer = rand.Next(1, 4)
+    Dim random As Integer = rand.Next(1, 4)
+    Dim switchtotal As Integer
+    Dim total As Integer = 1
+    Dim switches As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnDoor1.Click
+
+        If counter < 1 Then
+            choice = 1
+            While random2 = choice OrElse random2 = random
+                random2 = rand.Next(1, 4)
+            End While
+            If random2 = choice OrElse random2 = random Then
+                random2 = rand.Next(1, 4)
+            Else
+                If random2 = 2 Then
+                    PictureBox2.Visible = False
+                ElseIf random2 = 3 Then
+                    PictureBox3.Visible = False
+                End If
+            End If
+        ElseIf counter = 1 Then
+            choice2 = 1
+        End If
+        doors(choice, choice2)
+        counter += 1
+        If counter = 2 Then
+            PictureBox1.Visible = False
+            PictureBox2.Visible = False
+            PictureBox3.Visible = False
+        End If
+
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnDoor2.Click
+        If counter < 1 Then
+            choice = 2
+            While random2 = choice OrElse random2 = random
+                random2 = rand.Next(1, 4)
+            End While
+            If random2 = choice OrElse random2 = random Then
+                random2 = rand.Next(1, 4)
+            Else
+                If random2 = 1 Then
+                    PictureBox1.Visible = False
+                ElseIf random2 = 3 Then
+                    PictureBox3.Visible = False
+                End If
+            End If
+        ElseIf counter = 1 Then
+            choice2 = 2
+        End If
+        doors(choice, choice2)
+        counter += 1
+        If counter = 2 Then
+            PictureBox1.Visible = False
+            PictureBox2.Visible = False
+            PictureBox3.Visible = False
+        End If
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnDoor3.Click
+        If counter < 1 Then
+            choice = 3
+            While random2 = choice OrElse random2 = random
+                random2 = rand.Next(1, 4)
+            End While
+            If random2 = choice OrElse random2 = random Then
+                random2 = rand.Next(1, 4)
+            Else
+                If random2 = 1 Then
+                    PictureBox1.Visible = False
+                ElseIf random2 = 2 Then
+                    PictureBox2.Visible = False
+                End If
+            End If
+        ElseIf counter = 1 Then
+            choice2 = 3
+        End If
+        doors(choice, choice2)
+        counter += 1
+        If counter = 2 Then
+            PictureBox1.Visible = False
+            PictureBox2.Visible = False
+            PictureBox3.Visible = False
+        End If
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Random = 1 Then
+            PictureBox4.Image = My.Resources.th__1_
+            PictureBox5.Image = My.Resources.GOAT
+            PictureBox6.Image = My.Resources.GOAT
+        ElseIf Random = 2 Then
+            PictureBox4.Image = My.Resources.GOAT
+            PictureBox5.Image = My.Resources.th__1_
+            PictureBox6.Image = My.Resources.GOAT
+        ElseIf Random = 3 Then
+            PictureBox4.Image = My.Resources.GOAT
+            PictureBox5.Image = My.Resources.GOAT
+            PictureBox6.Image = My.Resources.th__1_
+        End If
+        If choice2 = random AndAlso Switchdoor = False Then
+            WIN += 1
+        ElseIf choice2 = random AndAlso Switchdoor = True Then
+            WIN += 1
+            switches += 1
+        ElseIf choice2 <> random AndAlso Switchdoor = False Then
+            loss += 1
+        ElseIf choice2 <> random AndAlso Switchdoor = True Then
+            loss += 1
+            switches += 1
+        End If
+        winscount.Text = WIN
+        switchcount.Text = switches
+        totalgames.Text = total
+    End Sub
+    Sub doors(Choice As Integer, Choice2 As Integer)
+        If Choice = 3 AndAlso Choice2 = 3 Then
+            PictureBox3.Visible = False
+            Switchdoor = False
+        ElseIf Choice = 3 AndAlso Choice2 = 2 Then
+            PictureBox2.Visible = False
+            Switchdoor = True
+        ElseIf Choice = 3 AndAlso Choice2 = 1 Then
+            PictureBox1.Visible = False
+            Switchdoor = True
+        ElseIf choice = 2 AndAlso choice2 = 2 Then
+            PictureBox2.Visible = False
+            Switchdoor = False
+        ElseIf choice = 2 AndAlso choice2 = 3 Then
+            PictureBox3.Visible = False
+            Switchdoor = True
+        ElseIf choice = 2 AndAlso choice2 = 1 Then
+            PictureBox1.Visible = False
+            Switchdoor = True
+        ElseIf choice = 1 AndAlso choice2 = 1 Then
+            PictureBox1.Visible = False
+            Switchdoor = False
+        ElseIf choice = 1 AndAlso choice2 = 2 Then
+            PictureBox2.Visible = False
+            Switchdoor = True
+        ElseIf choice = 1 AndAlso choice2 = 3 Then
+            PictureBox3.Visible = False
+            Switchdoor = True
+        End If
+    End Sub
+
+    Private Sub Reset_Click(sender As Object, e As EventArgs) Handles Reset.Click
+        counter = 0
+        PictureBox1.Visible = True
+        PictureBox2.Visible = True
+        PictureBox3.Visible = True
+        random = rand.Next(1, 4)
+        random2 = rand.Next(1, 4)
+        If random2 = random Then
+            random2 = rand.Next(1, 4)
+        End If
+        Switchdoor = False
+        choice = 0
+        choice2 = 0
+        If random = 1 Then
+            PictureBox4.Image = My.Resources.th__1_
+            PictureBox5.Image = My.Resources.GOAT
+            PictureBox6.Image = My.Resources.GOAT
+        ElseIf random = 2 Then
+            PictureBox4.Image = My.Resources.GOAT
+            PictureBox5.Image = My.Resources.th__1_
+            PictureBox6.Image = My.Resources.GOAT
+        ElseIf random = 3 Then
+            PictureBox4.Image = My.Resources.GOAT
+            PictureBox5.Image = My.Resources.GOAT
+            PictureBox6.Image = My.Resources.th__1_
+        End If
+        If random = choice2 AndAlso Switchdoor = False Then
+            WIN += 1
+        ElseIf random = choice2 AndAlso Switchdoor = True Then
+            switchWIN += 1
+        ElseIf random <> choice2 AndAlso Switchdoor = False Then
+            loss += 1
+        ElseIf random <> choice2 AndAlso Switchdoor = True Then
+            switchloss += 1
+        End If
+        total += 1
+        winscount.Text = WIN
+        switchcount.Text = switches
+        totalgames.Text = total
+    End Sub
+End Class
